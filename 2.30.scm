b93ef4820e3e5aee90ba100b891028a79589d123
floam <kasinovviktorr@mail.ru>
2026-03-12 19:10:42 +0300

    2.30 exercise

---
+++ 2.30.scm
@@ -0,0 +1,12 @@
+(define (square x) (* x x))
+
+(define (square_tree tree)
+    (map (lambda (sub_tree)
+        (if (pair? sub_tree)
+            (square_tree sub_tree)
+            (square sub_tree))) tree))
+
+(print (square_tree
+(list 1
+    (list 2 (list 3 4) 5)
+    (list 6 7))))
