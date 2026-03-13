07e5305f0ad369d10de40c7bdc30a511d2677064
floam <kasinovviktorr@mail.ru>
2026-03-13 09:49:42 +0300

    2.35 exercise

---
+++ 2.35.scm
@@ -0,0 +1,30 @@
+(define (accumulate operand initial sequence)
+    (if (null? sequence)
+        initial
+        (operand (car sequence) (accumulate operand initial (cdr sequence)))))
+
+
+; (define (enumerate_tree t)
+;     (cond
+;         ((pair? t) (append (enumerate_tree (car t)) (enumerate_tree (cdr t))))
+;         ((null? t) t)
+;         (else (list t))))
+
+
+
+
+
+
+(define (count_leaves t)
+    (accumulate
+        (lambda (x y) (+ x y))
+        0
+        (map 
+            (lambda (x) 
+                (cond
+                    ((null? x) 0)
+                    ((pair? x) (count_leaves x))
+                    (else 1)))
+            t)))
+
+(print (count_leaves (list (list 1 2 3 (list 1 2))  (list 1 2))))
